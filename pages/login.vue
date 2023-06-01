0aeecd7d8f50e7ff9a0c435695f410c96186be41
LÊ HỮU HOÀN <hoanle396@gmail.com>
2023-06-01 11:44:57 +0700

    Custom login

--- pages/login.vue
+++ pages/login.vue
@@ -1,46 +1,7 @@
 <template>
-    <div class="container">
-        <h1>Sign in to access the secret page</h1>
-        <div>
-            <label for="email">
-                <input id="email" type="email" value="test">
-            </label>
-            <label for="password">
-                <input id="password" type="password" value="test">
-            </label>
-            <button @click="postLogin">login</button>
-            <p>The credentials are not verified for the example purpose.</p>
-        </div>
-    </div>
+        <Login/>
 </template>
 
-<script >
-import { useAuth } from "~/stores/auth";
+<script lang="ts">
 
-const Cookie = process.client ? require("js-cookie") : undefined;
-
-export default {
-    middleware: "notAuthenticated",
-    setup() {
-        const { setAuth } = useAuth()
-        return {
-            setAuth
-        }
-    },
-    methods: {
-        postLogin: function () {
-            console.log("here");
-            const auth = {
-                accessToken: "someStringGotFromApiServiceWithAjax"
-            };
-            this.setAuth(auth)
-            Cookie.set("auth", JSON.stringify(auth));
-            if (this.$route.query.r) {
-                this.$router.push(decodeURIComponent(this.$route.query.r));
-            } else {
-                this.$router.push("/");
-            }
-        },
-    }
-};
 </script>
